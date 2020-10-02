d5af09fd7f849cea4d13ce51adcf153d4617faad
Nishant Yadav <nynishuyadav85@gmail.com>
2020-10-02 09:51:26 +0530

    Updates

---
+++ precache-manifest.8d73d3025155f38202ac94822fad1cb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d837df6f0cfadffc45bf1c90a99fb32a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4afa5b51c18c8adc9e49",
+    "url": "/weather-app/static/css/main.a4d654de.chunk.css"
+  },
+  {
+    "revision": "525d011ed98e4e898173",
+    "url": "/weather-app/static/js/2.4e8d7327.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4e8d7327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4afa5b51c18c8adc9e49",
+    "url": "/weather-app/static/js/main.7ecd39b5.chunk.js"
+  },
+  {
+    "revision": "24a1604f70ac33123060",
+    "url": "/weather-app/static/js/runtime-main.17d1d65d.js"
+  }
+]);
